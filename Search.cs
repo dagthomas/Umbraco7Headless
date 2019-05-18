46c9ef0cb6f158d86e091b4f7c9043ef80692930
dagthomas <dagthomas@gmail.com>
2019-05-18 08:38:32 +0200

    Added surface controllers
    add controllers, profit

---
+++ Search.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Examine;
+
+namespace LandeSpeiderWebSide.Controllers
+{
+    public class MySearcher
+    {
+        public ISearchResults Search(string keywords)
+        {
+            var searcher = ExamineManager.Instance.SearchProviderCollection["ExternalSearcher"];
+
+            var searchCriteria = searcher.CreateSearchCriteria(Examine.SearchCriteria.BooleanOperation.Or);
+
+            var rawQueries = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(keywords))
+            {
+                var words = keywords.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    var contentRawQuery = string.Format(word, 0.5);
+                    //var mediaRawQuery = string.Format("(+__IndexType:media -__NodeTypeAlias:folder && (nodeName:{0}~{1}))", word, 0.5);
+                    rawQueries.Add(contentRawQuery);
+                }
+            }
+            var query = searchCriteria.RawQuery("(" + String.Join(")(", rawQueries) + ")");
+
+            return searcher.Search(query);
+        }
+    }
+}
